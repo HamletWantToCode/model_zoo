60ae87b04fa5396ee9e234ef8e38c2a93b22b013
hongbinren <hongbinrenscu@outlook.com>
2020-02-22 16:07:23 +0800

    Files generated by PkgTemplates

---
+++ src/GP_model_zoo.jl
@@ -0,0 +1,5 @@
+module GP_model_zoo
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using GP_model_zoo
+using Test
+
+@testset "GP_model_zoo.jl" begin
+    # Write your own tests here.
+end
